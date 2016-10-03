0f8bcb8addfc1171dc0b6139dbef14ab94c83c01
Harry Grubman <grubmanh@asbindia.org>
2016-10-03 23:55:56 +0530

    Version 1.0.0
    Big jump to 1.0.0 here, lot’s of exciting new features. First, there is
    more than one cloud. All five clouds move in a cloud like pace, and
    wrap around the edges of the world. They move using vectors.
    Interpolation of variable names failed, so for statement did not work
    for making and using five clouds, area for growth.

--- sketch_88/build/source/sketch_88.java
+++ sketch_88/build/source/sketch_88.java
@@ -16,39 +16,111 @@ import java.io.IOException;
 
 public class sketch_88 extends PApplet {
 
-/*    Project 3 by Harry Grubman
-      This project is an exploration of movement on a generated world.
+/*    Unit 1 Final Project by Harry Grubman
+      This project is a combination of all the concepts learned in Unit 1.
+      It is randomly generated clouds moving with vectors in a generated world.
 */
-
+   // Import OCD Cam
 Camera camera;
-Cloud cloud1;
-float velocity, wallHeight;
-PImage sky;
-PImage wall;
-int worldSize = 500;
-
+Cloud cloud1, cloud2, cloud3, cloud4, cloud5;   //  Creates 5 clouds
+float velocity;
+int worldSize = 500;    // Square world of length and width worldSize
+int wallHeight = 100;   // Walls of height wallHeight
 
 public void setup()
 {
   
-  camera = new Camera(this, 10, -2, 10, 1, 1000);
+  camera = new Camera(this, 10, -5, 10, 1, 1000);
   cloud1 = new Cloud();
-  camera.aim(0, -2, 0);
-  wall = loadImage("wall.jpg");
-  // sky.resize(10,10);
+  cloud2 = new Cloud();
+  cloud3 = new Cloud();
+  cloud4 = new Cloud();
+  cloud5 = new Cloud();
+  camera.aim(worldSize, -5, worldSize);
 }
 public void draw()
 {
   background(0);
-  worldGen(worldSize, worldSize);
-  wallHeight = 100;
+  worldGen(worldSize, worldSize, wallHeight);   // Generated the square world of worldSize
   lights();
   camera.feed();
   camera.dolly(velocity);
-  fill(0xffffffff);
-  textSize(5);
-  cloud1.start(1);
-  text(velocity, 10, 0);
+  cloud1.render();    // Rendering and movement of clouds
+  cloud1.cloudMovement();
+  cloud2.render();
+  cloud2.cloudMovement();
+  cloud3.render();
+  cloud3.cloudMovement();
+  cloud4.render();
+  cloud4.cloudMovement();
+  cloud5.render();
+  cloud5.cloudMovement();
+  ocdMovement();      // Function for movement of OCD cam
+}
+public void worldGen(int worldWid, int worldDep, int wallHeight)
+{
+  beginShape(QUADS);   // Create world with custom shapes
+      fill(255);
+      vertex(0,0,0);              // Far Side of Cube
+      vertex(0,1,0);
+      vertex(worldWid,1, 0);
+      vertex(worldWid,0,0);
+
+      vertex(worldWid,0,0);         // Right Side of Cube
+      vertex(worldWid,1,0);
+      vertex(worldWid,1,worldDep);
+      vertex(worldWid,0,worldDep);
+
+      vertex(0,1,0);         // Top Side of Cube
+      vertex(0,1,worldDep);
+      vertex(worldWid,1,worldDep);
+      vertex(worldWid,1,0);
+
+      vertex(0,1,worldDep);    // Front Side of Cube
+      vertex(0,0,worldDep);
+      vertex(worldWid,0,worldDep);
+      vertex(worldWid,1,worldDep);
+
+      vertex(0,0,0);              // Left Side of Cube
+      vertex(0,0,worldDep);
+      vertex(0,1,worldDep);
+      vertex(0,1,0);
+      fill(0xff7CAF4C);
+      vertex(0,0,0);              // Bottom Side of Cube
+      vertex(0,0,worldDep);
+      vertex(worldWid,0,worldDep);
+      vertex(worldWid,0,0);
+      fill(0xffffffff);
+      vertex(0,1,0);              // Far Side of Wall
+      vertex(0,-(wallHeight + 1),0);
+      vertex(worldWid,-(wallHeight + 1),0);
+      vertex(worldWid,1,0);
+
+      vertex(worldWid,1,0);       // Right Side of Wall
+      vertex(worldWid,-(wallHeight + 1),0);
+      vertex(worldWid,-(wallHeight + 1),worldDep);
+      vertex(worldWid,1,worldDep);
+
+      vertex(0,1,0);              // Left Side of Wall
+      vertex(0,-(wallHeight + 1),0);
+      vertex(0,-(wallHeight + 1),worldDep);
+      vertex(0,1,worldDep);
+
+      vertex(0,1,worldDep);       // Near Side of Wall
+      vertex(0,-(wallHeight + 1),worldDep);
+      vertex(worldWid,-(wallHeight + 1), worldDep);
+      vertex(worldWid,1,worldDep);
+
+      fill(0xff99ccff);
+      vertex(0,-(wallHeight + 1),0);  // Sky of World
+      vertex(worldWid,-(wallHeight + 1),0);
+      vertex(worldWid,-(wallHeight + 1),worldDep);
+      vertex(0,-(wallHeight + 1),worldDep);
+
+  endShape();
+}
+public void ocdMovement()    // Move OCD cam with keyboard WASD keys
+{
   if(keyPressed == true)
   {
     if (key == 's')
@@ -75,115 +147,70 @@ public void draw()
     }
   }
 }
-public void worldGen(int worldWid, int worldDep)
+class Cloud   // Cloud object
 {
-  beginShape(QUADS);
-      fill(255);
-      vertex(0,0,0);              // Far Side of Cube
-      vertex(0,1,0);
-      vertex(worldWid,1, 0);
-      vertex(worldWid,0,0);
-
-      vertex(worldWid,0,0);         // Right Side of Cube
-      vertex(worldWid,1,0);
-      vertex(worldWid,1,worldDep);
-      vertex(worldWid,0,worldDep);
-
-      vertex(0,1,0);         // Top Side of Cube
-      vertex(0,1,worldDep);
-      vertex(worldWid,1,worldDep);
-      vertex(worldWid,1,0);
-
-      vertex(0,1,worldDep);    // Front Side of Cube
-      vertex(0,0,worldDep);
-      vertex(worldWid,0,worldDep);
-      vertex(worldWid,1,worldDep);
-
-      vertex(0,0,0);              // Left Side of Cube
-      vertex(0,0,worldDep);
-      vertex(0,1,worldDep);
-      vertex(0,1,0);
-
-      vertex(0,0,0);              // Bottom Side of Cube
-      vertex(0,0,worldDep);
-      vertex(worldWid,0,worldDep);
-      vertex(worldWid,0,0);
-
-      vertex(0,1,0);              // Far Side of Wall
-      vertex(0,-(wallHeight + 1),0);
-      vertex(worldWid,-(wallHeight + 1),0);
-      vertex(worldWid,1,0);
-
-      vertex(worldWid,1,0);       // Right Side of Wall
-      vertex(worldWid,-(wallHeight + 1),0);
-      vertex(worldWid,-(wallHeight + 1),worldDep);
-      vertex(worldWid,1,worldDep);
-
-      vertex(0,1,0);              // Left Side of Wall
-      vertex(0,-(wallHeight + 1),0);
-      vertex(0,-(wallHeight + 1),worldDep);
-      vertex(0,1,worldDep);
-
-      vertex(0,1,worldDep);       // Near Side of Wall
-      vertex(0,-(wallHeight + 1),worldDep);
-      vertex(worldWid,-(wallHeight + 1), worldDep);
-      vertex(worldWid,1,worldDep);
-
-      fill(0xff99ccff);
-      vertex(0,-(wallHeight + 1),0,                0,0);  // Sky of World
-      vertex(worldWid,-(wallHeight + 1),0,         0,10);
-      vertex(worldWid,-(wallHeight + 1),worldDep,  10,10);
-      vertex(0,-(wallHeight + 1),worldDep,         10,0);
-
-  endShape();
-}
-
-class Cloud
-{
-  float x_pos = 250.0f;
-  float y_pos = -2.0f;
-  float z_pos = 250.0f;
+  PVector location;
+  PVector velocity;
+  float y_pos = -90.0f;
   int cloudLength = 100;
   int cloudWidth = 50;
   int cloudHeight = -10;
+  float cloudXMinSpeed = 0.05f;
+  float cloudXMaxSpeed = 0.15f;
+  float cloudZMinSpeed = 0.05f;
+  float cloudZMaxSpeed = 0.15f;
 
-  public void start(int cloudNumber)
+  Cloud()
+  {
+    location = new PVector(random(0,worldSize), y_pos, random(0,worldSize));
+    velocity = new PVector(random(cloudXMinSpeed,cloudXMaxSpeed),0,random(cloudZMinSpeed,cloudZMaxSpeed));
+  }
+  public void render()
   {
     pushMatrix();
-    translate(x_pos, y_pos, z_pos);
+    translate(location.x, location.y, location.z); // Show cloud at position
     cloudShape();
     popMatrix();
   }
-
+  public void cloudMovement()
+  {
+    if (location.x < worldSize && location.z < worldSize) // Move cloud at velocity
+      location.add(velocity);
+    if (location.x > worldSize) // Keep cloud in x axis
+      location.add(-(worldSize + cloudWidth),0,0);
+    if (location.z > worldSize) // Keep cloud in y axis
+      location.add(0,0,-(worldSize + cloudLength));
+  }
   public void cloudShape()
   {
-    beginShape(QUADS);
-      fill(255);
+    noStroke();
+    beginShape(QUADS);  // Custom cloud shape
+      fill(0xffffffff);
       vertex(0,0,0);   // Bottom of Cloud
       vertex(0,0,cloudLength);
       vertex(cloudWidth,0,cloudLength);
       vertex(cloudWidth,0,0);
-
+      fill(0xffffffff);
       vertex(0,cloudHeight,0);   // Top of Cloud
       vertex(0,cloudHeight,cloudLength);
       vertex(cloudWidth,cloudHeight,cloudLength);
       vertex(cloudWidth,cloudHeight,0);
-
+      fill(0xffffffff);
       vertex(0,0,0);   // Left of Cloud
       vertex(0,0,cloudLength);
       vertex(0,cloudHeight,cloudLength);
       vertex(0,cloudHeight,0);
-
+      fill(0xffffffff);
       vertex(cloudWidth,0,0);    // Right of Cloud
       vertex(cloudWidth,0,cloudLength);
       vertex(cloudWidth,cloudHeight,cloudLength);
       vertex(cloudWidth,cloudHeight,0);
-
+      fill(0xffffffff);
       vertex(0,0,0);    // Front of Cloud
       vertex(0,cloudHeight,0);
       vertex(cloudWidth,cloudHeight,0);
       vertex(cloudWidth,0,0);
-
+      fill(0xffffffff);
       vertex(0,0,cloudLength);   //Back of Cloud
       vertex(0,cloudHeight,cloudLength);
       vertex(cloudWidth,cloudHeight,cloudLength);
